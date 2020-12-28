0e4a5f83dd3480bb479d47b62d6f7d15412845b8
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2020-12-28 23:48:22 +0700

    fix host

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const sequelize = require('sequelize')
 
-const db = new sequelize('expressAPI', 'fill', 'secret', {
+const db = new sequelize('dahb619dum3s10', 'fkmlsncdmwbnbg', '8c458aa6960652d65de46e2723bb31d74228048fac5530c3517379ee50cf325d', {
+	host: 'ec2-54-84-98-18.compute-1.amazonaws.com'
 	dialect: 'postgres'
 })
 
