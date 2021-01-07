974da3187e7a52f7a780f9b5630f032844b93d5e
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-01-07 14:15:59 +0700

    fix database

--- database.js
+++ database.js
@@ -11,7 +11,7 @@ const pg = require('pg')
 
 // heroku
 const db = new sequelize('dahb619dum3s10', 'fkmlsncdmwbnbg', '8c458aa6960652d65de46e2723bb31d74228048fac5530c3517379ee50cf325d', {
-	host: 'ec2-54-84-98-18.compute-1.amazonaws.com1',
+	host: 'ec2-54-84-98-18.compute-1.amazonaws.com',
 	dialect: 'postgres'
 })
 
